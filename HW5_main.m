9d2c506b49e10a30e52f70bbf18aa71c77b4d4a3
CarolXu0527 <xuzhenzhi2009@gmail.com>
2018-02-26 21:39:01 -0500

    Update HW5_main.m

--- HW5_main.m
+++ HW5_main.m
@@ -3,7 +3,12 @@ function HW5_main
     x = [0 1 2 3];
     y = [0 1 1 1];
     %HW5_LSCF_parabolic_lsqcurvefit(x, y);
-    HW5_LSCF_parabolic_fsolve(x, y);
+    %HW5_LSCF_parabolic_fsolve(x, y);
+    
+    x_2 = [1 2 3 4];
+    y_2 = [1 2 3 4];
+    %HW5_LSCF_exponential_lsqcurvefit(x_2, y_2);
+    HW5_LSCF_exponential_fsolve(x_2, y_2);
 
 return
 
@@ -28,15 +33,17 @@ function HW5_LSCF_parabolic_fsolve(feat, pred)
     y = pred;
     hold on
     plot(x, y, 'ro');
-    title('fitting using fminunc');
-
-    %f = @(c, xdata) c(1).*xdata.^2 + c(2).*xdata + c(3);
-    %Fsumsquares = @(c) sum((f(c, x) - y).^2);
+    title('fitting using fsolve');
     
-    syms L M N
-    f = L.*x.^2 + M.*x + N;
-    Fsumsquares = sum((f(L, M, N, x) - y).^2);
-    g = gradient(Fsumsquares, [L, M, N]);
+    syms a b c
+    
+    Fsumsquares = 0;
+    
+    for i = 1:length(x)
+        Fsumsquares = Fsumsquares + (y(i) - (a*x(i)^2 + b*x(i) + c))^2;
+    end
+    
+    g = gradient(Fsumsquares, [a, b, c]);
     fn = {matlabFunction(g(1)), matlabFunction(g(2)), matlabFunction(g(3))};
     
     fn1 = @(a)[fn{1}(a(1), a(2), a(3)), fn{2}(a(1), a(2), a(3)), fn{3}(a(1), a(2), a(3))]; 
@@ -46,14 +53,53 @@ function HW5_LSCF_parabolic_fsolve(feat, pred)
     
     a_min = fsolve(fn1, a_0);
     
-    %[xunc,ressquared,eflag,outputu] = fminunc(Fsumsquares,c_0,opts);
-    %disp(xunc);
-    %plot(x, y, '.-', x, f(xunc,x), 'r:x');
-    
     disp(a_min);
+    plot(x, y, '.-', x, a_min(1)*x.^2 + x.*a_min(2) + a_min(3), 'r:x');
     
 return
 
-function HW5_LSCF_exponential
+function HW5_LSCF_exponential_lsqcurvefit(feat, pred)
+    x = feat;
+    y = pred;
+    hold on
+    plot(x, y, 'ro');
+    title('fitting using lsqcurvefit');
+    
+    f = @(c, xdata) c(1).*(1 - exp(-c(2).*xdata));
+    c_0 = [1 1];
+    
+    c = lsqcurvefit(f,c_0,x,y);
+    disp(c);
+    plot(x, y, '.-', x, f(c,x), 'r:x');
 
+return
+
+function HW5_LSCF_exponential_fsolve(feat, pred)
+    x = feat;
+    y = pred;
+    hold on
+    plot(x, y, 'ro');
+    title('fitting using fsolve');
+    
+    syms a b
+    
+    Fsumsquares = 0;
+    
+    for i = 1:length(x)
+        Fsumsquares = Fsumsquares + (y(i) - (a.*(1 - exp(-b.*x(i)))))^2;
+    end
+    
+    g = gradient(Fsumsquares, [a, b]);
+    fn = {matlabFunction(g(1)), matlabFunction(g(2))};
+    
+    fn1 = @(a)[fn{1}(a(1), a(2)), fn{2}(a(1), a(2))]; 
+    
+    opts = optimoptions('fsolve','MaxFunctionEvaluations',40000, 'MaxIterations', 40000);
+    a_0 = [1 1];
+    
+    a_min = fsolve(fn1, a_0, opts);
+    
+    disp(a_min);
+    plot(x, y, '.-', x, a_min(1)*(1 - exp(-a_min(2).*x)), 'r:x');
+    
 return
