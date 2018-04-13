f7b247faa4990964c066f1cc9199f27875d10e5f
CarolXu0527 <xuzhenzhi2009@gmail.com>
2018-04-13 16:29:04 -0500

    Update HW9.m

--- HW9.m
+++ HW9.m
@@ -15,6 +15,8 @@ P_g = gradient(P, [D, H]);
 
 fcontour(P);
 grid on;
+axis([-100 100 -100 100]);
+hold on
 
 P_fun = matlabFunction(P);
 
@@ -23,10 +25,56 @@ s_D = matlabFunction(P_g(1));
 s_H = matlabFunction(P_g(2));
 
 % initial value
-D_cur = 100;
-H_cur = 100;
+D_cur = 500;
+H_cur = 500;
+D_pre = Inf;
+H_pre = Inf;
 
 P_cur = P_fun(D_cur, H_cur);
+P_pre = Inf;
+
+fprintf('wura');
+
+disp((abs(D_cur-D_pre)/D_pre < 0.01));
+disp((abs(D_cur-D_pre)/D_pre < 0.01));
+disp((abs(D_cur-D_pre)/D_pre < 0.01));
+
+while ((abs(D_cur-D_pre)/D_pre > 0.01) && (abs(H_cur-H_pre)/H_pre > 0.01)) || (abs(P_cur-P_pre)/P_pre > 0.01)
+    syms d
+
+    D_d = D_cur + d*(-1)*s_D(D_cur, H_cur);
+    H_d = H_cur + d*(-1)*s_H(D_cur, H_cur);
+    
+    P_d = pi*D_d*H_d + (pi/2)*D_d^2 + heaviside(-(D_d^2)*H_d*pi/4 + 500)*(-(D_d^2)*H_d*pi/4 + 500)^2;
+    Pd_fun = matlabFunction(P_d);
+
+    d_star = fminunc(Pd_fun, 0);
+    
+    D_fun = matlabFunction(D_d);
+    D_star = D_fun(d_star);
+    
+    H_fun = matlabFunction(H_d);
+    H_star = H_fun(d_star);
+    
+    P_star = Pd_fun(d_star);
+    
+    disp(['D_star: ' num2str(D_star)]);
+    disp(['H_star: ' num2str(H_star)]);
+    disp(['P_star: ' num2str(P_star)]);
+    scatter(D_star, H_star, 'b');
+    plot([D_cur, D_star], [H_cur, H_star], '-k');
+    text(D_star, H_star, ['D = ' num2str(D_star) ' H = ' num2str(H_star)]);
+    
+    D_pre = D_cur;
+    H_pre = H_cur;
+    
+    D_cur = D_star;
+    H_cur = H_star;
+    
+    fprintf('\n %.2f, %.2f, %.2f', D_cur, H_cur, P_star);
+end
+    
+    
 
 
 
